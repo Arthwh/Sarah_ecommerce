50f21471c5048aeb979bf6ff0dce0bd463ade37e
yuuta <felipenoll04@gmail.com>
2024-10-08 13:56:27 -0300

    Remoção da /api no get do updateProductVariant

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -9,6 +9,6 @@ router.get('/products/:id', ProductController.getProduct);
 router.post('/products', ProductController.createProduct);
 router.put('/products/:id', ProductController.updateProduct);
 router.delete('/products/:id', ProductController.deleteProduct);
-router.get("/api/products/updateProductVariant/:id", ProductController.updateProductVariantData)
+router.get('/products/updateProductVariant/:id', ProductController.updateProductVariantData);
 
 export default router;

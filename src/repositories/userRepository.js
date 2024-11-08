587868c72518ec46b90871f5a4fc33d4443f457f
Arthur Gabriel Fabrim Alonso <arthur.fabrin028@gmail.com>
2024-11-07 21:58:40 -0300

    Corrigido erro na criação de usuário

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -75,7 +75,7 @@ class UserRepository {
                 FROM shopping_cart_items sci
                 JOIN shopping_carts sc ON sc.id = sci.shopping_cart_id
                 WHERE sc.user_id = $1`, [userId]);
-            return parseInt(rows[0].count, 10);
+            return parseInt(rows[0].count);
         } catch (error) {
             console.error('Error getting cart count:', error);
             throw error;
